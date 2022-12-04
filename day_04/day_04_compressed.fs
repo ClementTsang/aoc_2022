ef236703207e1984056a7a5a1937e881e6cb5f21
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2022-12-04 05:31:06 -0500

    why

--- day_04/day_04_compressed.fs
+++ day_04/day_04_compressed.fs
@@ -3,53 +3,16 @@
 64 constant max-line
 create line-buf max-line chars allot
 
-: clone_four
-    4 0 do
-        3 roll dup 
-        3 0 do 
-            4 roll swap
-        loop 
-    loop
-;
-
-: part_one_check
-    clone_four
-    rot <= -rot <= and
-    if drop drop drop drop -1 else 3 roll 3 roll rot <= -rot <= and then
-;
-
-: part_two_check
-    -rot >= -rot <= and
-;
-
-: load_line
-    4 0 do 
-        0 0
-        2swap
-        >number
-        1 - swap 1 + swap 2swap drop
-        -rot 
-    loop
-    drop drop
-;
-
 : main
     0 0
     begin
         line-buf max-line stdin read-line throw
     while
-        line-buf swap load_line clone_four
-
-        part_one_check
-        6 roll swap if 1 + else then 4 0 do 4 roll loop
-
-        part_two_check
-        2 roll swap if 1 + else then
-    repeat
-    drop swap
-    ." Part 1: " . cr
-    ." Part 2: " . cr
+        line-buf swap 4 0 do 0 0 2swap >number 1 - swap 1 + swap 2swap drop -rot loop drop drop 4 0 do 3 roll dup 3 0 do 4 roll swap loop loop \ Load line, prep
+        4 0 do 3 roll dup 3 0 do 4 roll swap loop loop rot <= -rot <= and if drop drop drop drop -1 else 3 roll 3 roll rot <= -rot <= and then 6 roll swap if 1 + else then 4 0 do 4 roll loop \ Part 1
+        -rot >= -rot <= and 2 roll swap if 1 + else then \ Part 2
+    repeat drop swap
+    ." Part 1: " . cr ." Part 2: " . cr
 ;
-
 main
 bye
